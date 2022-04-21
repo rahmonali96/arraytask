35073c19edbc4871334143d88c7e5dea790de606
Rahmonali <rahmonaliyoqubov@gmail.com>
2022-04-21 20:45:55 +0500

    4

--- src/main/java/uz/epam/arraytask/util/CustomArrayCreator.java
+++ src/main/java/uz/epam/arraytask/util/CustomArrayCreator.java
@@ -4,6 +4,7 @@ import uz.epam.arraytask.entity.CustomArray;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CustomArrayCreator {
     public List<CustomArray> customArrayList(List<int[]> numbers) {
@@ -12,5 +13,14 @@ public class CustomArrayCreator {
             customArrayList.add(new CustomArray(ints));
         }
         return customArrayList;
+    }
+
+    public CustomArray createCustomArray(int size) {
+        Random random = new Random();
+        int[] ints = new int[size];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = random.nextInt(10);
+        }
+        return new CustomArray(ints);
     }
 }
